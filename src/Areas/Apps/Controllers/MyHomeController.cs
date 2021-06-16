84d103e54f33e85896e941a971b2c573c2cc9843
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-06-16 15:55:37 +0800

    home page added

---
+++ src/Areas/Apps/Controllers/MyHomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weavy.Areas.Apps.Models;
+using Weavy.Core.Models;
+using Weavy.Core.Services;
+using Weavy.Web.Controllers;
+
+namespace Weavy.Areas.Apps.Controllers
+{
+    public class MyHomeController : AppController
+    {
+        // GET: aviation-marketplace
+        [Route("aviation-marketplace")]
+        public ActionResult Index()
+        {
+            var joined = SpaceService.GetVisited(10);
+
+            var pods = SpaceService.Search(new SpaceQuery { Tag = "pods", Top = 10 });
+            var gigs = SpaceService.Search(new SpaceQuery { Tag = "gigs", Top = 10 });
+            var pubs = SpaceService.Search(new SpaceQuery { Top = 10 });
+
+            var notifications = NotificationService.Search(new NotificationQuery
+            {
+                OrderBy = "Id DESC",
+                SearchRead = null,
+                Top = PageSizes.First(),
+                
+            }).ToList();
+            var stars = EntityService.Search(new EntityQuery
+            {
+                StarredById = User.Id
+            }).ToList();
+            MyHomeViewModel viewModel = new MyHomeViewModel
+            {
+                JoinedSpaces = joined,
+                PodsSpaces = pods.Where(x => x.Tags.Any(y => y.ToLower() == "pods")),
+                GigsSpaces = gigs.Where(x => x.Tags.Any(y => y.ToLower() == "gigs")),
+                PubSpaces = pubs.ToList(),
+                Notifications = notifications,
+                Stars = stars
+            };
+
+            return View("~/Areas/Apps/Views/MyHome/Index.cshtml", viewModel);
+        }
+    }
+}
---
+++ src/Areas/Apps/Models/MyHomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Weavy.Core.Models;
+
+namespace Weavy.Areas.Apps.Models
+{
+    public class MyHomeViewModel
+    {
+        public MyHomeViewModel()
+        {
+            JoinedSpaces = new List<Space>();
+            PodsSpaces = new List<Space>();
+            GigsSpaces = new List<Space>();
+            PubSpaces = new List<Space>();
+            Notifications = new List<Notification>();
+            Stars = new List<Entity>();
+        }
+        public IEnumerable<Space> JoinedSpaces { get; set; }
+        public IEnumerable<Space> PodsSpaces { get; set; }
+        public IEnumerable<Space> GigsSpaces { get; set; }
+        public IEnumerable<Space> PubSpaces { get; set; }
+        public IEnumerable<Notification> Notifications { get; set; }
+        public IEnumerable<Entity> Stars { get; set; }
+    }
+}
