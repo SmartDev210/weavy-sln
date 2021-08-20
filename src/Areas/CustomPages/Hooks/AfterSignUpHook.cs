2e34daa11694f24dfc1c18c5a9bbd4490270b1ef
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-08-21 03:57:49 +0800

    welcome message from bryan to singup users

--- src/Areas/CustomPages/Hooks/AfterSignUpHook.cs
+++ src/Areas/CustomPages/Hooks/AfterSignUpHook.cs
@@ -15,10 +15,10 @@ namespace Weavy.Areas.CustomPages.Hooks
     [Serializable]
     [Guid("F9EAF019-1ADC-46A0-8A29-3C9FC866ABAF")]
     [Plugin(Description = "A hook that automatically add new users to global spaces")]
-    public class AfterSignInHook : Hook, IHook<AfterInsertUser>
+    public class AfterSignUpHook : Hook, IHook<AfterInsertUser>
     {
         /// <summary>
-        /// A hook that automatically add users to global spaces
+        /// A hook that automatically add users to global spaces, join to companies, send message from bryan to new user
         /// </summary>
         /// <param name="e"></param>
         public void Handle(AfterInsertUser e)
@@ -37,6 +37,23 @@ namespace Weavy.Areas.CustomPages.Hooks
 
             var techSupport = SpaceService.GetByKey("tech-support", true);
             if (techSupport != null && !techSupport.IsMember) { SpaceService.AddMember(techSupport.Id, e.Inserted.Id, Access.Write, true); }
+
+            var bryan = UserService.GetByEmail("support@findparts.aero");
+            if (bryan != null)
+            {
+                var conversation = ConversationService.Insert(new Conversation() { CreatedById = bryan.Id }, new int[] { e.Inserted.Id });
+
+                var message = $@"<p>Welcome to AvDB,</p>"
+                    + $@"<p>Please look around, search & connect</p>"
+                    + $@"<p>you can direct message me here!</p>"
+                    + $@"<p>@bryan</p>";
+
+                MessageService.Insert(new Message
+                {
+                    CreatedById = bryan.Id,
+                    Html = message
+                }, conversation, sudo: true);
+            }
 
             var searchResult = SpaceService.Search(new SpaceQuery { });
             var iter = searchResult.GetEnumerator();
