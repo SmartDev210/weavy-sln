d6b06b5add4d2246d2711f0e2dc7791e56121a62
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-10-20 17:04:55 +0800

    public spaces issue fixed

--- src/Areas/CustomPages/Controllers/MyHomeController.cs
+++ src/Areas/CustomPages/Controllers/MyHomeController.cs
@@ -85,7 +85,7 @@ namespace Weavy.Areas.CustomPages.Controllers
         public ActionResult ClubhouseAviationMarketplace()
         {
             var joined = SpaceService.Search(new SpaceQuery { Text="tag:collab", Top = 100, Sudo = true, MemberId = User.Id });
-            var pubs = SpaceService.Search(new SpaceQuery { Text = "tag:master", Top = 100, Sudo = true, MemberId = User.Id });
+            var pubs = SpaceService.Search(new SpaceQuery { Text = "tag:master", Top = 100, Sudo = true });
 
             ClubhouseAviationMarketplaceHomePageViewModel viewModel = new ClubhouseAviationMarketplaceHomePageViewModel
             {
